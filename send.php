dfd9679cbf4c0edfd16937d45204991b066761ea
MikeArvalor <mihail14312@mail.ru>
2020-12-07 22:59:13 +0300

    sait

--- send.php
+++ send.php
@@ -1,27 +1,24 @@
-<?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$tel=$_POST['tel'];
-$message=$_POST['message'];
-$name=htmlspecialchars($name);
-$email=htmlspecialchars($email);
-$tel=htmlspecialchars($tel);
-$message=htmlspecialchars($message);
-
-$name=urldecode($name);
-$email=urldecode($email);
-$tel=urldecode($tel);
-$message=urldecode($message);
-
-$name=trim($name);
-$email=trim($email);
-$tel=trim($tel);
-$message=trim($message);
-if(mail("mihail14312@mail.ru","новое письмо с сайта",
-"Name: ".$name."\n"."Email: ".$email."\n"."Telephone: ".$tel,"From:\r\n")
-){
-    echo("yes");
-}
-else{echo("no");}
-    
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+$tel=$_POST['tel'];
+$message=$_POST['message'];
+
+$name=htmlspecialchars($name);
+$email=htmlspecialchars($email);
+$tel=htmlspecialchars($tel);
+$message=htmlspecialchars($message);
+
+$name=urldecode($name);
+$email=urldecode($email);
+$tel=urldecode($tel);
+$message=urldecode($message);
+
+$name=trim($name);
+$email=trim($email);
+$tel=trim($tel);
+$message=trim($message);
+mail("mail@londoncityassociates.com","London City Associates Ltd",
+"Name: ".$name."\n"."Email: ".$email."\n"."Telephone: ".$tel."\n"."Message: ".$message,"From:\r\n")
+
 ?>
